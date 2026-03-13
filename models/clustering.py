d96296b9a2277d9f524fbbb99aa77edfdae0ff8d
ZhouNeil <yz9695@nyu.edu>
2026-03-13 13:30:12 -0400

    Add MerchantClustering class for K-Means clustering
    Initial implementation of MerchantClustering class with placeholders for fit and predict methods.

---
+++ models/clustering.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class MerchantClustering:
+    """
+    Handles K-Means clustering for discovering commercial zones.
+    Will be implemented from scratch using Numpy later.
+    """
+    def __init__(self, n_clusters=5):
+        self.n_clusters = n_clusters
+        
+    def fit(self, data):
+        # Placeholder for the clustering logic
+        pass
+        
+    def predict(self, data):
+        # Placeholder for prediction
+        pass
